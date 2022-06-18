569e72f8b4152f4f24cccfb06e45a3e56bab5f6f
RealVG <107572070+RealVG@users.noreply.github.com>
2022-06-18 19:29:43 +0200

    aa

--- bot.py
+++ bot.py
@@ -138,7 +138,7 @@ async def clearchat(ctx, amount=100000000):
 
 @bot.command(description="manda mess privati")
 async def msg(ctx,user,*,msg):
-        user=bot.get_user(user_id)
+        user=bot.get_user(userid)
         await bot.send_message(user,msg)
 
 
